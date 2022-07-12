b5058033314bf60906c402f716b02d78ff7b5cab
Sergey <s_kulaev@bk.ru>
2022-07-12 17:11:11 +0700

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { SignupModule } from './pages/signup/signup.module';
 import { API_URL } from './urls-tokens';
-import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, SignupModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SignupModule,
+    NgbModule,
+  ],
   providers: [
     {
       provide: API_URL,
